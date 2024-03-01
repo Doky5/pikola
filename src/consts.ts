aeb8d62fcd687df347714373d2b450c639e248f9
Doky5 <jdokulil5@gmail.com>
2024-03-01 18:46:00 +0100

    Its getting somewhere

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Hra na pikanou';
+export const SITE_DESCRIPTION = 'Schovat se může být hodně těžké!';
